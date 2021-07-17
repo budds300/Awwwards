649f0f3e573074d148e72269b67a52ce1b2fa790
budds300 <budds300@gmail.com>
2021-07-17 18:57:56 +0300

    update of search,id and profile url

--- awwards/urls.py
+++ awwards/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from django.urls import  path
 from . import views
 
+
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('search/',views.search_result,name='search_result'),
+    path('project/<project_id>',views.project,name = 'project'),
+    path('profile/',views.profile, name='profile')
 ]
