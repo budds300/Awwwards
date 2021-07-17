55cb58e45cddc50404dffb4a63c8e83339a014bf
budds300 <budds300@gmail.com>
2021-07-17 18:56:37 +0300

    addition of login and logout url

--- awards/urls.py
+++ awards/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('awwards.urls'))
+    path('' , include('awwards.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout')
 ]
