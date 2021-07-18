22215bd5aedcc97f03c92b2402153ee53efc6a29
budds300 <budds300@gmail.com>
2021-07-18 10:42:59 +0300

    addition of register,profile,update,upload and rate urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('search/',views.search_result,name='search_result'),
     path('project/<project_id>',views.project,name = 'project'),
-    path('profile/',views.profile, name='profile')
+    path('profile/',views.profile, name='profile'),
+    path('register/',views.register, name='register'),
+    path('update/',views.update, name='update'),
+    path('upload_project/',views.upload_project,name='upload_project'),
+    path('rate/<project_id>/',views.rate,name='rate')
 ]
